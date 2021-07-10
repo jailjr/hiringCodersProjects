64823399bacae1e1979da4a47ab025d0e479633f
jailjr <jjgsj@cin.ufpe.br>
2021-07-10 19:50:41 -0300

    Todo list done

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -38,5 +38,54 @@ function celciusToFahrenheit(temperature){
     return (temperature*(9/5) + 32);
 }
 
-let result = celciusToFahrenheit(30);
-console.log(result);
+
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let lists = []
+/*[{
+    id: 1,
+    name: 'js'
+}, {
+    id:2,
+    name:'html'
+},{
+    id:3,
+    name:'css'}]
+*/
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
+function render(){
+    clearElement(listContainer)
+    lists.forEach(function(list){
+        const item = document.createElement('li')
+        item.classList.add('item')
+        item.innerText = list.name
+        listContainer.appendChild(item)
+    })
+}
+
+function createList(name){
+    return {id: Date.now().toString(), name: name}
+}
+
+
+newListForm.addEventListener('submit',  function(e) {
+    e.preventDefault()
+    const listName = newListInput.value
+    if(listName === null || listName === '') return
+
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+
+    render()
+})
+
+
+

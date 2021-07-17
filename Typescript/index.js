5206ca9a115a6c0fc92e1891af5940c1cda05b41
jailjr <jjgsj@cin.ufpe.br>
2021-07-17 17:21:43 -0300

    Typescript studies started

---
+++ Typescript/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+var numero1 = 10;
+var numero2 = 5;
+var result = somar(numero1, numero2);
+console.log(result);
+console.log("DAORA");
---
+++ Typescript/index.ts
@@ -0,0 +1,11 @@
+function somar(numero1: number, numero2: number)
+{
+    return numero1 + numero2;
+}
+
+const numero1: number = 10;
+const numero2 = 5;
+
+const result = somar(numero1, numero2);
+console.log(result);
+console.log("DAORA");
